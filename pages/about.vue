4f4553d7ae6a71773c83047ab8a9ad5e415fd052
colekins <colekins@gmail.com>
2020-05-11 19:26:02 -0400

    Update About section text

--- pages/about.vue
+++ pages/about.vue
@@ -7,10 +7,10 @@
         alt="dust logo"
       />
       <p>
-        Dust, Etc. is a tiny record label in Boston, MA.
+        Dust, Etc. is a tiny record label in Portland, ME.
       </p>
       <p>
-        Dust, Etc. was founded by Cole Kinsler in October, 2015.
+        Dust, Etc. was founded by Cole Kinsler in 2015.
       </p>
       <p>
         <a href="mailto:dustetc@gmail.com?Subject=Howdy" target="_blank"
@@ -36,5 +36,6 @@ export default {
 <style>
 .about {
   text-align: center;
+  font-size: 18px;
 }
 </style>
