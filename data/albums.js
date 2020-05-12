160130500c8d86e2849a4cad42203947ae02487b
colekins <colekins@gmail.com>
2020-05-12 18:01:35 -0400

    Start adding album JSON

--- data/albums.js
+++ data/albums.js
@@ -3,9 +3,28 @@ const data = {
     {
       title: 'togetherness',
       artist: 'space mountain',
+      releaseDate: 'March 8, 2019',
+      embed:
+        '<iframe style="border: 0; width: 100%; height: 370px;" src="https://bandcamp.com/EmbeddedPlayer/album=756941381/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless><a href="http://spacemountain.bandcamp.com/album/togetherness">Togetherness by Space Mountain</a></iframe>',
+      review: {
+        body:
+          'If an answer is found, then it exists within the confessional yet outward looking tone, dwelling on personal fears not as some exercise in self-absorption but instead a channel for empathy and solidarity. Being human might be difficult, but that doesn’t mean we should lose our humanity.',
+        author: 'Jon Doyle',
+        link: [
+          'Various Small Flames',
+          'https://varioussmallflames.co.uk/2019/01/10/space-mountain-cloud/'
+        ]
+      },
+      links: {
+        bandcamp: 'https://spacemountain.bandcamp.com/album/togetherness',
+        spotify:
+          '<iframe class="spotify" src="https://open.spotify.com/follow/1/?uri=spotify:artist:1X9FdQzGc1UmGJNNLmOZf8&size=basic&theme=light" width="100" height="25" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowtransparency="true"></iframe>'
+      },
       images: {
         small: 'https://f4.bcbits.com/img/a2453144117_2.jpg',
-        tape: 'https://f4.bcbits.com/img/0015236380_16.jpg'
+        tape: 'https://f4.bcbits.com/img/0015236380_16.jpg',
+        cd: null,
+        vinyl: null
       }
     },
     {
