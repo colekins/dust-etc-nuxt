343f5dfc7de2041cad17c7572f5dae6b268ee75d
colekins <colekins@gmail.com>
2020-05-12 18:02:24 -0400

    Remove unused styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -42,7 +42,7 @@ body {
 
 .fade-enter-active,
 .fade-leave-active {
-  transition-duration: 0.3s;
+  transition-duration: 0.4s;
   transition-property: opacity;
   transition-timing-function: ease;
 }
@@ -50,34 +50,5 @@ body {
 .fade-enter,
 .fade-leave-active {
   opacity: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
