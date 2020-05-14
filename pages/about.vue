0e550b51207f95b17c50ec00a478e7cb84fcce87
colekins <colekins@gmail.com>
2020-05-14 15:14:50 -0400

    Add image for about section

--- pages/about.vue
+++ pages/about.vue
@@ -1,24 +1,26 @@
 <template>
-  <b-container>
-    <div class="about">
-      <img
-        src="https://i.imgur.com/XouIPox.jpg"
-        width="300 px"
-        alt="dust logo"
-      />
-      <p>
-        Dust, Etc. is a tiny record label in Portland, ME.
-      </p>
-      <p>
-        Dust, Etc. was founded by Cole Kinsler in 2015.
-      </p>
-      <p>
-        <a href="mailto:dustetc@gmail.com?Subject=Howdy" target="_blank"
-          >dustetc@gmail.com</a
-        >
-      </p>
-      <br />
-    </div>
+  <b-container class="about">
+    <b-row>
+      <b-col lg="6" class="about-text">
+        <img
+          src="https://i.imgur.com/XouIPox.jpg"
+          width="300 px"
+          alt="dust logo"
+        />
+        <p>
+          Dust, Etc. is a tiny record label in Portland, ME.<br />
+          Dust, Etc. was founded by Cole Kinsler in 2015. <br />
+          <a href="mailto:dustetc@gmail.com?Subject=Hi" target="_blank"
+            >dustetc@gmail.com</a
+          >
+        </p>
+        <p></p>
+        <br />
+      </b-col>
+      <b-col lg="6">
+        <img src="~/assets/cole.jpg" width="100%" />
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -35,7 +37,11 @@ export default {
 
 <style>
 .about {
-  margin-top: 8vh;
+  margin-top: 2em;
+  margin-bottom: 4em;
+}
+.about-text {
+  margin-top: 3em;
   text-align: center;
   font-size: 17px;
 }
