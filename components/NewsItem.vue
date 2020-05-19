33f32165fcd290580f289993c55eec82db370ce7
colekins <colekins@gmail.com>
2020-05-18 21:43:56 -0400

    Now iterating over posts for News page

---
+++ components/NewsItem.vue
@@ -0,0 +1,41 @@
+/* eslint-disable vue/no-v-html */
+<template>
+  <b-row>
+    <b-col class="news-item">
+      <h2>
+        {{ post.title.split('-')[0] }} -
+        <span class="album-title">{{ post.title.split('-')[1] }}</span>
+      </h2>
+      <p class="news" v-html="$md.render(post.body)"></p>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    post: Object
+  }
+}
+</script>
+
+<style>
+.album-row {
+  margin-bottom: 20px;
+  margin-left: -15px;
+  margin-right: -15px;
+}
+
+.album-title {
+  font-style: italic;
+  font-weight: bold;
+}
+
+blockquote {
+  padding: 10px 20px;
+  margin: 0 0 20px;
+  font-size: 15.5px;
+  border-left: 5px solid #eee;
+}
+</style>
---
+++ pages/News.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container class="container news-container">
+    <NewsItem v-for="post in posts" :key="post.title" :post="post"> </NewsItem>
+  </b-container>
+</template>
+
+<script>
+import NewsItem from '~/components/NewsItem.vue'
+
+export default {
+  components: {
+    NewsItem
+  },
+  computed: {
+    posts() {
+      return this.$store.state.blogPosts
+    }
+  }
+}
+</script>
+
+<style>
+.news-container {
+  margin-top: 3em;
+  margin-bottom: 4em;
+}
+</style>
