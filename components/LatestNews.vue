2ea1b44ce3155f845a9ae15b43384181a41032dc
colekins <colekins@gmail.com>
2020-05-18 21:25:09 -0400

    Update latest news to reference store

--- components/LatestNews.vue
+++ components/LatestNews.vue
@@ -1,36 +1,14 @@
+/* eslint-disable vue/no-v-html */
 <template>
   <div>
     <div class="section-title">News</div>
     <div class="news-item">
-      <!-- <h2>Kerem Atalay - <span class="album-title">Summer Winter</span></h2> -->
-      <h2>{{ news.title }}</h2>
+      <h2>
+        {{ news.title.split('-')[0] }} -
+        <span class="album-title">{{ news.title.split('-')[1] }}</span>
+      </h2>
 
-      <p class="news">
-        {{ news.body }}
-      </p>
-
-      <blockquote class="quote-card">
-        <p>
-          "Summer Winter stems from the southern blues of Fahey’s earlier work.
-          Split into two seasonal sides—“Summer” and “Winter,” four parts
-          each—Atalay asks listeners to lean in closely to spot the subtle
-          differences in how his guitar, all acoustic with no effects,
-          translates hot and cool. The familiar ways to differentiate the
-          seasons are present. “Summer” is often faster, louder, and has a
-          warmer tone, while “Winter” is cooler in tone, quiet, and slower. The
-          trap with these traditional fingerpicked methods is that one could
-          make “Winter” less complex and less compelling. Atalay avoids this
-          with a clever choice: He tweaks the weight of the notes."
-        </p>
-        <cite>
-          - Justin Weber,
-          <a
-            href="https://www.washingtoncitypaper.com/arts/music/blog/21010437/kerem-atalays-summer-winter-reviewed"
-            target="_blank"
-            >Washington City Paper</a
-          >
-        </cite>
-      </blockquote>
+      <p class="news" v-html="$md.render(news.body)"></p>
       <iframe
         style="border: 0; width: 100%; height: 340px;"
         src="https://bandcamp.com/EmbeddedPlayer/album=303252568/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/"
